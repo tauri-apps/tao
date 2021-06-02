bbe04f7bb8870e193dbb5dbcde8075f249409fee
David Lemarier <david@lemarier.ca>
2021-06-02 16:08:53 -0400

    create windows examples — left click open or focus window

--- examples/system_tray.rs
+++ examples/system_tray.rs
@@ -19,12 +19,12 @@ fn main() {
     event_loop::{ControlFlow, EventLoop},
     menu::{ContextMenu as Menu, MenuType},
     system_tray::SystemTrayBuilder,
-    window::Window,
+    window::{Window, WindowId},
   };
 
   SimpleLogger::new().init().unwrap();
   let event_loop = EventLoop::new();
-  let mut windows = HashMap::new();
+  let mut windows: HashMap<WindowId, Window> = HashMap::new();
 
   let mut tray_menu = Menu::new();
 
@@ -76,51 +76,75 @@ fn main() {
   event_loop.run(move |event, event_loop, control_flow| {
     *control_flow = ControlFlow::Wait;
 
+    let mut create_window_or_focus = || {
+      // if we already have one window, let's focus instead
+      if !windows.is_empty() {
+        for window in windows.values() {
+          window.set_focus();
+        }
+        return ();
+      }
+
+      // create new window
+      let mut open_new_window_element = open_new_window_element.clone();
+      let mut focus_all_window = focus_all_window.clone();
+
+      let window = Window::new(&event_loop).unwrap();
+      windows.insert(window.id(), window);
+      // disable button
+      open_new_window_element.set_enabled(false);
+      // change title (text)
+      open_new_window_element.set_title("Window already open");
+      // set checked
+      open_new_window_element.set_selected(true);
+      // enable focus window
+      focus_all_window.set_enabled(true);
+      // update tray icon
+      system_tray.set_icon(new_icon.clone());
+      // add macOS Native red dot
+      #[cfg(target_os = "macos")]
+      open_new_window_element.set_native_image(NativeImage::StatusUnavailable);
+    };
+
     match event {
       Event::WindowEvent { event, window_id } => {
         if event == WindowEvent::CloseRequested {
           let mut open_new_window_element = open_new_window_element.clone();
-          println!("Window {:?} has received the signal to close", window_id);
           // Remove window from our hashmap
           windows.remove(&window_id);
-          // Enable our button
+          // Modify our button's state
           open_new_window_element.set_enabled(true);
           focus_all_window.set_enabled(false);
           // Reset text
           open_new_window_element.set_title("Open new window");
-          // Set unchecked
+          // Set selected
           open_new_window_element.set_selected(false);
+          // Change tray icon
           system_tray.set_icon(icon.clone());
+          // macOS have native image available that we can use in our menu-items
           #[cfg(target_os = "macos")]
           open_new_window_element.set_native_image(NativeImage::StatusAvailable);
         }
       }
+      // on Windows, habitually, we show the window with left click
+      #[cfg(target_os = "windows")]
+      Event::TrayEvent {
+        event: tao::event::TrayEvent::LeftClick,
+        ..
+      } => create_window_or_focus(),
+      // left click on menu item
       Event::MenuEvent {
         menu_id,
+        // specify only context menu's
         origin: MenuType::ContextMenu,
       } => {
-        let mut open_new_window_element = open_new_window_element.clone();
-        if menu_id == open_new_window_element.clone().id() {
-          let window = Window::new(&event_loop).unwrap();
-          windows.insert(window.id(), window);
-          // disable button
-          open_new_window_element.set_enabled(false);
-          // change title (text)
-          open_new_window_element.set_title("Window already open");
-          // set checked
-          open_new_window_element.set_selected(true);
-          // enable focus window
-          focus_all_window.set_enabled(true);
-          // update tray icon
-          system_tray.set_icon(new_icon.clone());
-          #[cfg(target_os = "macos")]
-          open_new_window_element.set_native_image(NativeImage::StatusUnavailable);
+        // Click on Open new window or focus item
+        if menu_id == open_new_window_element.clone().id()
+          || menu_id == focus_all_window.clone().id()
+        {
+          create_window_or_focus();
         }
-        if menu_id == focus_all_window.clone().id() {
-          for window in windows.values() {
-            window.set_focus();
-          }
-        }
+        // click on `quit` item
         if menu_id == quit_element.clone().id() {
           // on windows, we make sure to remove the icon from the tray
           // it require the `SystemTrayExtWindows`
