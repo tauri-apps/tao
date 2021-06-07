adae9ebb16e40c057e14e91a42a47a769be2e8e6
David Lemarier <david@lemarier.ca>
2021-06-07 11:58:37 -0400

    update `hotkey` example

--- examples/hotkey.rs
+++ examples/hotkey.rs
@@ -8,27 +8,18 @@ fn main() {
     dpi::LogicalSize,
     event::{ElementState, Event, KeyEvent, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
+    hotkey::{HotKey, RawMods},
     keyboard::{Key, ModifiersState},
+    platform::modifier_supplement::KeyEventExtModifierSupplement,
     window::WindowBuilder,
   };
 
-  use tao::platform::modifier_supplement::KeyEventExtModifierSupplement;
-
   SimpleLogger::new().init().unwrap();
 
-  fn handle_key_event(modifiers: ModifiersState, event: KeyEvent) {
-    if event.state == ElementState::Pressed
-      && !event.repeat
-      // catch only 1 without the modifier, we'll match later...
-      && event.key_without_modifiers() == Key::Character("1".to_string())
-    {
-      if modifiers.shift_key() {
-        println!("Shift + 1 | logical_key: {:?}", event.logical_key);
-      } else {
-        println!("1");
-      }
-    }
-  }
+  // create a sample hotkey
+  let hotkey = HotKey::new(RawMods::Shift, "1");
+  // create local modifier state
+  let mut modifiers = ModifiersState::default();
 
   let event_loop = EventLoop::new();
 
@@ -37,8 +28,6 @@ fn main() {
     .build(&event_loop)
     .unwrap();
 
-  let mut modifiers = ModifiersState::default();
-
   event_loop.run(move |event, _, control_flow| {
     *control_flow = ControlFlow::Wait;
 
@@ -46,12 +35,24 @@ fn main() {
       match event {
         WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
         WindowEvent::ModifiersChanged(new_state) => {
-          println!("new_state {:?}", new_state);
+          // update our local modifier state
           modifiers = new_state;
         }
-        WindowEvent::KeyboardInput { event, .. } => {
-          println!("event {:?}", event);
-          handle_key_event(modifiers, event);
+        // catch only pressed event
+        WindowEvent::KeyboardInput { event, .. } if event.state == ElementState::Pressed => {
+          // match with Hotkey, important to use `key_without_modifiers`
+          // otherwise `Shift` + `1` = `!`
+          if hotkey.matches(&modifiers, &event.key_without_modifiers()) {
+            println!(
+              "KeyEvent:  `Shift` + `1` | logical_key: {:?}",
+              &event.logical_key
+            );
+            // we can match manually without `HotKey`
+          } else if event.key_without_modifiers() == Key::Character("1".to_string())
+            && modifiers.is_empty()
+          {
+            println!("KeyEvent: `1`");
+          }
         }
         _ => (),
       }
