e6a0e32895a4f510d4167b122d46812c2bd7ddd7
David Lemarier <david@lemarier.ca>
2021-06-09 16:33:06 -0400

    fmt

--- examples/global_shortcut.rs
+++ examples/global_shortcut.rs
@@ -1,7 +1,6 @@
 // Copyright 2019-2021 Tauri Programme within The Commons Conservancy
 // SPDX-License-Identifier: Apache-2.0
 
-
 #[cfg(any(target_os = "windows", target_os = "linux", target_os = "macos"))]
 fn main() {
   use simple_logger::SimpleLogger;
