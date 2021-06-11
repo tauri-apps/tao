cbf536104dae8f8176d064cefa26472254804e65
David Lemarier <david@lemarier.ca>
2021-06-11 08:52:11 -0400

    fix headers

--- src/platform_impl/windows/clipboard.rs
+++ src/platform_impl/windows/clipboard.rs
@@ -1,3 +1,6 @@
+// Copyright 2019-2021 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+
 use crate::clipboard::{ClipboardFormat, FormatId};
 use std::{
   ffi::{CString, OsStr},
