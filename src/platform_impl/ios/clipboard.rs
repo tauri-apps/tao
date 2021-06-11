93cd2dc78ed3caea0fbbf617b80b424e03734f11
David Lemarier <david@lemarier.ca>
2021-06-11 08:53:08 -0400

    fix `ios` and `android` build

--- src/platform_impl/ios/clipboard.rs
+++ src/platform_impl/ios/clipboard.rs
@@ -5,5 +5,7 @@
 pub struct Clipboard;
 impl Clipboard {
   pub fn write_text(&mut self, s: impl AsRef<str>) {}
-  pub fn read_text(&self) -> Option<String> {}
+  pub fn read_text(&self) -> Option<String> {
+    None
+  }
 }
