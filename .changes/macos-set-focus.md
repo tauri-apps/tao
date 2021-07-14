3da167aad9dad8ec2e3b3af52175a74a5ef07b99
david <david@lemarier.ca>
2021-07-14 13:08:25 -0400

    fix(macos): `set_focus` should be triggered even if the window isn't visible (#128)

---
+++ .changes/macos-set-focus.md
@@ -0,0 +1,5 @@
+---
+"tao": minor
+---
+
+On macOS, make sure the `set_focus` is triggered even if the window is not visible.
--- src/platform_impl/macos/window.rs
+++ src/platform_impl/macos/window.rs
@@ -462,9 +462,7 @@ impl UnownedWindow {
   pub fn set_focus(&self) {
     unsafe {
       let is_minimized: BOOL = msg_send![*self.ns_window, isMiniaturized];
-      let is_visible: BOOL = msg_send![*self.ns_window, isVisible];
-
-      if is_minimized == NO && is_visible == YES {
+      if is_minimized == NO {
         util::set_focus(*self.ns_window);
       }
     }
