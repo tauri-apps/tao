18d61d002ae6a4305876f6136ee56e1e1ed7682d
amrbashir <48618675+amrbashir@users.noreply.github.com>
2021-07-22 08:52:29 +0200

    update comment

--- src/menu.rs
+++ src/menu.rs
@@ -32,7 +32,7 @@ use crate::{
 ///
 /// ## Platform-specific
 ///
-/// **Windows:** The native menu is destroyed when this stuct is dropped and can no longer be used.
+/// **Windows:** The native menu is destroyed when this struct is dropped and can no longer be used.
 pub struct ContextMenu(pub(crate) Menu);
 /// Object that allows you to create a `MenuBar`, menu.
 ///
@@ -40,7 +40,7 @@ pub struct ContextMenu(pub(crate) Menu);
 ///
 /// **macOs:** The menu will show in the **Menu Bar**.
 /// **Linux / Windows:** The menu will be show at the top of the window.
-/// **Windows:** The native menu is destroyed when this stuct is dropped and can no longer be used.
+/// **Windows:** The native menu is destroyed when this struct is dropped and can no longer be used.
 pub struct MenuBar(pub(crate) Menu);
 
 /// A custom menu item.
