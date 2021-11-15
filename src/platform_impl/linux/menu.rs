b047ae41a83b94cb140cb3a3decd1ddb4ea6d405
Kasper <kasperkh.kh@gmail.com>
2021-11-15 07:22:18 +0100

    feat: support accelerator key strings `,` `-` `.` `Space` `Tab` and `F13`-`F24` (#228)
    * feat(macOS): support more accelerator key strings
    
    * Move function keys together
    
    * Add `,` `-` `.` `Space` `F20-F24` for Windows
    
    * Remove support for accelerators not found in `winapi`
    
    * Add `,` `-` `.` `Space` `F13-F24` for Linux
    
    * Update .changes
    
    * Add the rest for Windows
    
    * Add the rest for Linux
    
    * Add the rest on macOS
    
    * Update accelerator-strings.md

--- src/platform_impl/linux/menu.rs
+++ src/platform_impl/linux/menu.rs
@@ -305,6 +305,18 @@ fn register_accelerator(item: &GtkMenuItem, accel_group: &AccelGroup, menu_key: 
     KeyCode::Digit7 => '7' as u32,
     KeyCode::Digit8 => '8' as u32,
     KeyCode::Digit9 => '9' as u32,
+    KeyCode::Comma => ',' as u32,
+    KeyCode::Minus => '-' as u32,
+    KeyCode::Period => '.' as u32,
+    KeyCode::Space => ' ' as u32,
+    KeyCode::Equal => '=' as u32,
+    KeyCode::Semicolon => ';' as u32,
+    KeyCode::Slash => '/' as u32,
+    KeyCode::Backslash => '\\' as u32,
+    KeyCode::Quote => '\'' as u32,
+    KeyCode::Backquote => '`' as u32,
+    KeyCode::BracketLeft => '[' as u32,
+    KeyCode::BracketRight => ']' as u32,
     k => {
       if let Some(gdk_key) = key_to_raw_key(k) {
         *gdk_key
