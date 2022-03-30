fd24d20f62daeab6fe6f7688cd3903e37bb02c34
amrbashir <amr.bashir2015@gmail.com>
2022-03-31 00:29:32 +0200

    fix(Windows): fix random chars when changing menu item title

--- src/platform_impl/windows/event_loop.rs
+++ src/platform_impl/windows/event_loop.rs
@@ -589,7 +589,7 @@ lazy_static! {
         RegisterWindowMessageA("Tao::SetRetainMaximized")
     };
     static ref THREAD_EVENT_TARGET_WINDOW_CLASS: Vec<u16> = unsafe {
-        let mut class_name= util::to_wstring("Tao Thread Event Target");
+        let mut class_name= util::encode_wide("Tao Thread Event Target");
 
         let class = WNDCLASSEXW {
             cbSize: mem::size_of::<WNDCLASSEXW>() as u32,
