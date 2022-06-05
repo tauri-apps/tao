c5f5a775694b7e238821ae2584ad9d4fc536567c
amrbashir <amr.bashir2015@gmail.com>
2022-06-05 23:13:35 +0200

    typo

--- src/menu.rs
+++ src/menu.rs
@@ -398,7 +398,7 @@ impl CustomMenuItem {
   }
 
   // todo: Add set_icon
-  // pub fn set_icon(&mut self, icon: Vec<u8>, with: u32, height: u32) {
+  // pub fn set_icon(&mut self, icon: Vec<u8>) {
   //   self.0.set_icon(icon)
   // }
 }
