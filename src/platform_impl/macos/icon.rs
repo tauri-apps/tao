b8c4f98bfb0d9da693f881be04fd17c0da179cd6
amrbashir <amr.bashir2015@gmail.com>
2022-06-06 12:23:41 +0200

    again

--- src/platform_impl/macos/icon.rs
+++ src/platform_impl/macos/icon.rs
@@ -1,5 +1,5 @@
 use crate::icon::{BadIcon, RgbaIcon};
-use std::io::BufWriter;
+use std::io::Cursor;
 
 #[derive(Debug, Clone)]
 pub struct PlatformIcon(RgbaIcon);
@@ -10,15 +10,17 @@ impl PlatformIcon {
   }
 
   pub fn to_png(&self) -> Vec<u8> {
-    let png = Vec::new();
-    let ref mut w = BufWriter::new(png);
+    let mut png = Vec::new();
 
-    let mut encoder = png::Encoder::new(w, self.0.width as _, self.0.height as _);
-    encoder.set_color(png::ColorType::Rgba);
-    encoder.set_depth(png::BitDepth::Eight);
+    {
+      let mut encoder =
+        png::Encoder::new(Cursor::new(&mut png), self.0.width as _, self.0.height as _);
+      encoder.set_color(png::ColorType::Rgba);
+      encoder.set_depth(png::BitDepth::Eight);
 
-    let mut writer = encoder.write_header().unwrap();
-    writer.write_image_data(&self.0.rgba).unwrap();
+      let mut writer = encoder.write_header().unwrap();
+      writer.write_image_data(&self.0.rgba).unwrap();
+    }
 
     png
   }
