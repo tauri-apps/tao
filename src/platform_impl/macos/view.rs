54062ca1a96e3637acee771e9657ef0267933352
Ngo Iok Ui (Wu Yu Wei) <wusyong9104@gmail.com>
2022-06-22 18:59:38 +0800

    fix: emit resize event on frame_did_change on macOS, closes #436 (#439)
    * On macOS, emit resize event on frame_did_change
    
    When the window switches mode from normal to tabbed one, it doesn't
    get resized, however the frame gets resized. This commit makes
    winit to track resizes when frame changes instead of window.
    
    Co-authored-by: Phillip Hellewell <sshock@users.noreply.github.com>

--- src/platform_impl/macos/view.rs
+++ src/platform_impl/macos/view.rs
@@ -20,7 +20,7 @@ use objc::{
 };
 
 use crate::{
-  dpi::LogicalPosition,
+  dpi::{LogicalPosition, LogicalSize},
   event::{
     DeviceEvent, ElementState, Event, MouseButton, MouseScrollDelta, TouchPhase, WindowEvent,
   },
@@ -373,6 +373,16 @@ extern "C" fn frame_did_change(this: &Object, _sel: Sel, _event: id) {
         userData:ptr::null_mut::<c_void>()
         assumeInside:NO
     ];
+
+    // Emit resize event here rather than from windowDidResize because:
+    // 1. When a new window is created as a tab, the frame size may change without a window resize occurring.
+    // 2. Even when a window resize does occur on a new tabbed window, it contains the wrong size (includes tab height).
+    let logical_size = LogicalSize::new(rect.size.width as f64, rect.size.height as f64);
+    let size = logical_size.to_physical::<u32>(state.get_scale_factor());
+    AppState::queue_event(EventWrapper::StaticEvent(Event::WindowEvent {
+      window_id: WindowId(get_window_id(state.ns_window)),
+      event: WindowEvent::Resized(size),
+    }));
 
     state.tracking_rect = Some(tracking_rect);
   }
