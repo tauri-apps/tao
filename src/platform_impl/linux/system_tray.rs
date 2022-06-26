f9e510f70dd287882e9576ea6226948dbb8ec57e
Beanow <497556+Beanow@users.noreply.github.com>
2022-06-26 19:05:13 +0200

    linux: Add an argument to `temp_icon_path` for a custom directory

--- src/platform_impl/linux/system_tray.rs
+++ src/platform_impl/linux/system_tray.rs
@@ -34,7 +34,7 @@ impl SystemTrayBuilder {
     let mut app_indicator = AppIndicator::new("tao application", "");
 
     let (parent_path, icon_path) =
-      temp_icon_path().expect("Failed to create a temp folder for icon");
+      temp_icon_path(None).expect("Failed to create a temp folder for icon");
 
     self.icon.inner.write_to_png(&icon_path);
 
@@ -69,7 +69,7 @@ pub struct SystemTray {
 impl SystemTray {
   pub fn set_icon(&mut self, icon: Icon) {
     let (parent_path, icon_path) =
-      temp_icon_path().expect("Failed to create a temp folder for icon");
+      temp_icon_path(None).expect("Failed to create a temp folder for icon");
     icon.inner.write_to_png(&icon_path);
 
     self
@@ -98,28 +98,38 @@ impl Drop for SystemTray {
   }
 }
 
-fn temp_icon_path() -> std::io::Result<(PathBuf, PathBuf)> {
-  let mut parent_path = dirs_next::runtime_dir().unwrap_or_else(|| std::env::temp_dir());
+/// Generates an icon path in one of the following dirs:
+/// 1. If `temp_icon_dir` is `Some` use that.
+/// 2. `$XDG_RUNTIME_DIR/tao`
+/// 3. `/tmp/tao`
+fn temp_icon_path(temp_icon_dir: Option<&PathBuf>) -> std::io::Result<(PathBuf, PathBuf)> {
+  let parent_path = match temp_icon_dir.as_ref() {
+    Some(path) => path.to_path_buf(),
+    None => dirs_next::runtime_dir()
+      .unwrap_or_else(|| std::env::temp_dir())
+      .join("tao"),
+  };
 
-  parent_path.push("tao");
   std::fs::create_dir_all(&parent_path)?;
-  let mut icon_path = parent_path.clone();
-  icon_path.push(format!("tray-icon-{}.png", uuid::Uuid::new_v4()));
+  let icon_path = parent_path.join(format!("tray-icon-{}.png", uuid::Uuid::new_v4()));
   Ok((parent_path, icon_path))
 }
 
 #[test]
-fn temp_icon_path_prefers_runtime() {
+fn temp_icon_path_preference_order() {
   let runtime_dir = option_env!("XDG_RUNTIME_DIR");
+  let override_dir = PathBuf::from("/tmp/tao-tests");
 
-  let (dir1, _file1) = temp_icon_path().unwrap();
+  let (dir1, _file1) = temp_icon_path(Some(&override_dir)).unwrap();
+  let (dir2, _file1) = temp_icon_path(None).unwrap();
   std::env::remove_var("XDG_RUNTIME_DIR");
-  let (dir2, _file2) = temp_icon_path().unwrap();
+  let (dir3, _file2) = temp_icon_path(None).unwrap();
 
+  assert_eq!(dir1, override_dir);
   if let Some(runtime_dir) = runtime_dir {
     std::env::set_var("XDG_RUNTIME_DIR", runtime_dir);
-    assert_eq!(dir1, PathBuf::from(format!("{}/tao", runtime_dir)));
+    assert_eq!(dir2, PathBuf::from(format!("{}/tao", runtime_dir)));
   }
 
-  assert_eq!(dir2, PathBuf::from("/tmp/tao"));
+  assert_eq!(dir3, PathBuf::from("/tmp/tao"));
 }
