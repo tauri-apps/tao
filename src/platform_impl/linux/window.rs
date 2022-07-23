076ea7f0058c25d66497df8a8fd89698556bff27
Amr Bashir <amr.bashir2015@gmail.com>
2022-07-23 19:27:40 +0200

    please no more

--- src/platform_impl/linux/window.rs
+++ src/platform_impl/linux/window.rs
@@ -682,8 +682,9 @@ impl Window {
   pub fn raw_display_handle(&self) -> RawDisplayHandle {
     let mut display_handle = XlibDisplayHandle::empty();
     if let Ok(xlib) = x11_dl::xlib::Xlib::open() {
-      display_handle.display = (xlib.XOpenDisplay)(std::ptr::null()) as _;
-      display_handle.screen = (xlib.XDefaultScreen)(display_handle.display) as _;
+      let display = (xlib.XOpenDisplay)(std::ptr::null());
+      display_handle.display = display as _;
+      display_handle.screen = (xlib.XDefaultScreen)(display) as _;
     }
 
     RawDisplayHandle::Xlib(display_handle)
