ddb9f2bcdfaab745429d153c64ed6b823ac72f3c
Caesar Schinas <caesar@caesarschinas.com>
2022-09-29 22:17:52 +0100

    feat(window): examples for `minimizable` and `closable`

--- examples/minimize.rs
+++ examples/minimize.rs
@@ -5,7 +5,7 @@
 extern crate tao;
 
 use tao::{
-  event::{Event, WindowEvent},
+  event::{Event, WindowEvent, ElementState},
   event_loop::{ControlFlow, EventLoop},
   keyboard::Key,
   window::WindowBuilder,
@@ -17,7 +17,7 @@ fn main() {
   let event_loop = EventLoop::new();
 
   let window = WindowBuilder::new()
-    .with_title("A fantastic window!")
+    .with_title("Hit `m` to minimize, `space` to toggle minimizability")
     .build(&event_loop)
     .unwrap();
 
@@ -26,20 +26,26 @@ fn main() {
 
     match event {
       Event::WindowEvent {
-        event: WindowEvent::CloseRequested,
-        ..
-      } => *control_flow = ControlFlow::Exit,
-
-      // Keyboard input event to handle minimize via a hotkey
-      Event::WindowEvent {
-        event: WindowEvent::KeyboardInput { event, .. },
+        event,
         window_id,
         ..
-      } if window_id == window.id() && Key::Character("m") == event.logical_key => {
-        // Pressing the 'm' key will minimize the window
-        // WARNING: Consider using `key_without_modifers()` if available on your platform.
-        // See the `key_binding` example
-        window.set_minimized(true);
+      } if window_id == window.id() => match event {
+        WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+
+        // Keyboard input event to handle minimize via a hotkey
+        WindowEvent::KeyboardInput { event, .. } if event.state == ElementState::Pressed => match event.logical_key {
+          // Pressing the 'm' key will minimize the window
+          // WARNING: Consider using `key_without_modifers()` if available on your platform.
+          // See the `key_binding` example
+          Key::Character("m") => window.set_minimized(true),
+          Key::Space => {
+            let minimizable = !window.is_minimizable();
+            println!("Minimizable: {}", minimizable);
+            window.set_minimizable(minimizable);
+          }
+          _ => (),
+        }
+        _ => (),
       }
       _ => (),
     }
