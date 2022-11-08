64cff56435b859771688690fbfc801da99aa8776
amrbashir <amr.bashir2015@gmail.com>
2022-11-09 00:47:49 +0200

    fix(Windows): fix build regression when tray feature is used

---
+++ .changes/fix-compile-regression.md
@@ -0,0 +1,5 @@
+---
+"tao": "patch"
+---
+
+On Windows, fix compliation regression introduced in 0.15.1 when `tray` feature is active
--- src/platform_impl/windows/mod.rs
+++ src/platform_impl/windows/mod.rs
@@ -97,6 +97,8 @@ impl DeviceId {
 #[non_exhaustive]
 #[derive(Debug)]
 pub enum OsError {
+  #[allow(unused)]
+  CreationError(&'static str),
   IoError(std::io::Error),
 }
 impl std::error::Error for OsError {}
@@ -104,6 +106,7 @@ impl std::error::Error for OsError {}
 impl std::fmt::Display for OsError {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
     match self {
+      OsError::CreationError(e) => f.pad(e),
       OsError::IoError(e) => f.pad(&e.to_string()),
     }
   }
