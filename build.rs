f1d460b78468a4783ca0b8aa2a9c07cb5aff4189
amrbashir <amr.bashir2015@gmail.com>
2023-03-14 18:22:43 +0200

    feat: remove linking to `ColorSync`

--- build.rs
+++ build.rs
@@ -3,14 +3,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 fn main() {
-  // If building for macos and TAO_LINK_COLORSYNC is set to true
-  // use CGDisplayCreateUUIDFromDisplayID from ColorSync instead of CoreGraphics
-  if std::env::var("CARGO_CFG_TARGET_OS").map_or(false, |os| os == "macos")
-    && std::env::var("TAO_LINK_COLORSYNC")
-      .map_or(false, |v| v == "1" || v.eq_ignore_ascii_case("true"))
-  {
-    println!("cargo:rustc-cfg=use_colorsync_cgdisplaycreateuuidfromdisplayid");
-  }
   // link carbon hotkey on macOS
   {
     if std::env::var("CARGO_CFG_TARGET_OS").map_or(false, |os| os == "macos") {
