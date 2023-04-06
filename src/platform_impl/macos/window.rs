d974776f8fbba4ac0064f78a87caf4c09d150147
Alexander Haas <mail@haas-alexander.com>
2023-04-06 18:45:11 +0200

    Update src/platform_impl/macos/window.rs
    Co-authored-by: Amr Bashir <amr.bashir2015@gmail.com>
    Signed-off-by: amrbashir <amr.bashir2015@gmail.com>

--- src/platform_impl/macos/window.rs
+++ src/platform_impl/macos/window.rs
@@ -1420,7 +1420,7 @@ pub(crate) unsafe fn position_traffic_lights<V: NSWindow + Copy>(window: V, x: f
   let title_bar_frame_height = close_rect.size.height + y;
   let mut title_bar_rect = title_bar_container_view.frame();
   title_bar_rect.size.height = title_bar_frame_height;
-  title_bar_rect.origin.y = NSWindow::frame(window).size.height - title_bar_frame_height;
+  title_bar_rect.origin.y = window.frame().size.height - title_bar_frame_height;
   let _: () = msg_send![title_bar_container_view, setFrame: title_bar_rect];
 
   let window_buttons = vec![close, miniaturize, zoom];
