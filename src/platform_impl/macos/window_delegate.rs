3add4d4b9011657093620892aeae50b828d1f96e
Wu Wayne <yuweiwu@pm.me>
2023-05-04 15:14:50 +0800

    On macOS, fix windows frozen when starting in fullscreen

--- src/platform_impl/macos/window_delegate.rs
+++ src/platform_impl/macos/window_delegate.rs
@@ -341,18 +341,7 @@ extern "C" fn window_will_close(this: &Object, _: Sel, _: id) {
 extern "C" fn window_did_resize(this: &Object, _: Sel, _: id) {
   trace!("Triggered `windowDidResize:`");
   with_state(this, |state| {
-    // If window is entering/exiting, resize and move event will be emitted in
-    // window_did_enter_fullscreen & window_did_exit_fullscreen.
-    let in_fullscreen_transition = state
-      .with_window(|window| {
-        trace!("Locked shared state in `window_did_resize`");
-        let shared_state = window.shared_state.lock().unwrap();
-        trace!("Unlocked shared state in `window_did_resize`");
-        shared_state.in_fullscreen_transition
-      })
-      .unwrap_or(false);
-
-    if !state.is_checking_zoomed_in && !in_fullscreen_transition {
+    if !state.is_checking_zoomed_in {
       state.emit_resize_event();
       state.emit_move_event();
     }
