92520a72d9c355579b49ed9a3ee8e5e481d8f195
Géraud-Loup <47665233+geraudloup@users.noreply.github.com>
2023-07-26 09:57:37 +0000

    On Windows, set name of Window Class, closes #769 allow to customize it instead of current value hard coded "Window Class"

--- src/platform/windows.rs
+++ src/platform/windows.rs
@@ -231,6 +231,9 @@ pub trait WindowBuilderExtWindows {
   /// Whether to create the window icon with the taskbar icon or not.
   fn with_skip_taskbar(self, skip: bool) -> WindowBuilder;
 
+  /// Customize the window class name.
+  fn with_window_classname<S: Into<String>>(self, classname: S) -> WindowBuilder;
+
   /// Shows or hides the background drop shadow for undecorated windows.
   ///
   /// The shadow is hidden by default.
@@ -281,6 +284,12 @@ impl WindowBuilderExtWindows for WindowBuilder {
   #[inline]
   fn with_skip_taskbar(mut self, skip: bool) -> WindowBuilder {
     self.platform_specific.skip_taskbar = skip;
+    self
+  }
+
+  #[inline]
+  fn with_window_classname<S: Into<String>>(mut self, classname: S) -> WindowBuilder {
+    self.platform_specific.window_classname = classname.into();
     self
   }
 
