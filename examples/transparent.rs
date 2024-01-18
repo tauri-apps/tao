852b4147246fdce268d50aad2bee11e21e562930
amrbashir <amr.bashir2015@gmail.com>
2024-01-18 04:25:57 +0200

    chore(examples): showcase usage of softbuffer in transparent example

--- examples/transparent.rs
+++ examples/transparent.rs
@@ -2,6 +2,8 @@
 // Copyright 2021-2023 Tauri Programme within The Commons Conservancy
 // SPDX-License-Identifier: Apache-2.0
 
+use std::{num::NonZeroU32, rc::Rc};
+
 use tao::{
   event::{Event, WindowEvent},
   event_loop::{ControlFlow, EventLoop},
@@ -19,6 +21,14 @@ fn main() {
     .build(&event_loop)
     .unwrap();
 
+  #[cfg(windows)]
+  let (window, _context, mut surface) = {
+    let window = Rc::new(window);
+    let context = softbuffer::Context::new(window.clone()).unwrap();
+    let surface = softbuffer::Surface::new(&context, window.clone()).unwrap();
+    (window, context, surface)
+  };
+
   window.set_title("A fantastic window!");
 
   event_loop.run(move |event, _, control_flow| {
@@ -30,6 +40,25 @@ fn main() {
         event: WindowEvent::CloseRequested,
         ..
       } => *control_flow = ControlFlow::Exit,
+
+      #[cfg(windows)]
+      Event::RedrawRequested(_) => {
+        let (width, height) = {
+          let size = window.inner_size();
+          (size.width, size.height)
+        };
+        surface
+          .resize(
+            NonZeroU32::new(width).unwrap(),
+            NonZeroU32::new(height).unwrap(),
+          )
+          .unwrap();
+
+        let mut buffer = surface.buffer_mut().unwrap();
+        buffer.fill(0);
+        buffer.present().unwrap();
+      }
+
       _ => (),
     }
   });
