c7bae912a03f0203f15d08d03a9e7f54215c2d7c
Jason Tsai <jason@pews.dev>
2024-04-03 14:19:57 +0800

    fix(macos): race condition when toggling traffic light

---
+++ .changes/fix-mac-style-mask-race-condition.md
@@ -0,0 +1,5 @@
+---
+"tao": patch
+---
+
+On macOS, fix an issue where the set minimize maximize and close buttons would interfere with each other.
--- src/platform_impl/macos/window.rs
+++ src/platform_impl/macos/window.rs
@@ -744,7 +744,7 @@ impl UnownedWindow {
       } else {
         mask &= !NSWindowStyleMask::NSResizableWindowMask;
       }
-      self.set_style_mask_async(mask);
+      self.set_style_mask_sync(mask);
     } // Otherwise, we don't change the mask until we exit fullscreen.
   }
 
@@ -756,7 +756,7 @@ impl UnownedWindow {
     } else {
       mask &= !NSWindowStyleMask::NSMiniaturizableWindowMask;
     }
-    self.set_style_mask_async(mask);
+    self.set_style_mask_sync(mask);
   }
 
   #[inline]
@@ -777,7 +777,7 @@ impl UnownedWindow {
     } else {
       mask &= !NSWindowStyleMask::NSClosableWindowMask;
     }
-    self.set_style_mask_async(mask);
+    self.set_style_mask_sync(mask);
   }
 
   pub fn set_cursor_icon(&self, cursor: CursorIcon) {
