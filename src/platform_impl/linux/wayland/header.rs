a96dd7fd968b837d7b913667b173ff8f8bc65999
Chooooooo <contact@choo.ooo>
2024-10-04 20:42:36 +0900

    Update the title that affects the inner size

--- src/platform_impl/linux/wayland/header.rs
+++ src/platform_impl/linux/wayland/header.rs
@@ -1,7 +1,8 @@
 use gtk::{
   glib::{self},
+  pango,
   prelude::*,
-  ApplicationWindow, EventBox, HeaderBar,
+  Align, ApplicationWindow, EventBox, HeaderBar, Label,
 };
 
 pub struct WlHeader;
@@ -11,8 +12,12 @@ impl WlHeader {
     let header = HeaderBar::builder()
       .show_close_button(true)
       .decoration_layout("menu:minimize,maximize,close")
-      .title(title)
       .build();
+
+    let title_label = Label::new(Some(title));
+    title_label.set_ellipsize(gtk::pango::EllipsizeMode::End);
+    title_label.set_single_line_mode(true);
+    title_label.set_halign(Align::Center);
 
     let event_box = EventBox::new();
     event_box.set_above_child(true);
@@ -27,9 +32,28 @@ impl WlHeader {
       header_clone.set_size_request(min_width, allocated_height);
     });
 
+    header.set_custom_title(Some(&title_label));
     event_box.add(&header);
     window.set_titlebar(Some(&event_box));
+
+    //Set title font width
+    let context = title_label.pango_context();
+    let font_description = context.font_description().unwrap();
+    let font_size = (font_description.size() / pango::SCALE) as f64;
+    let char_width = font_size * 2.0;
+
+    Self::connect_configure_event(window, &title_label, char_width);
     Self::connect_resize_window(&header, window);
+  }
+
+  fn connect_configure_event(window: &ApplicationWindow, title_label: &Label, char_width: f64) {
+    let title_label_clone = title_label.clone();
+    window.connect_configure_event(move |_, event| {
+      let (width, _) = event.size();
+      let max_chars = (width as f64 / char_width).floor() as i32;
+      title_label_clone.set_max_width_chars(if width < 220 { 0 } else { max_chars });
+      false
+    });
   }
 
   fn connect_resize_window(header: &HeaderBar, window: &ApplicationWindow) {
