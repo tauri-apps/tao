5be29097db4b712242cc677da468e94062ecc73a
FabianLars <github@fabianlars.de>
2025-02-21 17:38:24 +0100

    mac 1

--- examples/overlay.rs
+++ examples/overlay.rs
@@ -2,7 +2,6 @@
 // Copyright 2021-2023 Tauri Programme within The Commons Conservancy
 // SPDX-License-Identifier: Apache-2.0
 
-use std::env::current_dir;
 use tao::{
   event::{ElementState, Event, KeyEvent, WindowEvent},
   event_loop::{ControlFlow, EventLoop},
