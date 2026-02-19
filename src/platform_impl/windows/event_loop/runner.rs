e8251c7c383bdee899d27e01c2cb25ac52403ed1
Thomas Eizinger <thomas@eizinger.io>
2026-02-19 14:26:52 +1100

    fix: do not panic on state transitions from `Destroyed`

--- src/platform_impl/windows/event_loop/runner.rs
+++ src/platform_impl/windows/event_loop/runner.rs
@@ -365,7 +365,9 @@ impl<T> EventLoopRunner<T> {
         self.call_event_handler(Event::LoopDestroyed);
       }
 
-      (Destroyed, _) => panic!("cannot move state from Destroyed"),
+      (Destroyed, state) => {
+        debug!("Eventloop is already destroyed, cannot move to {state}")
+      }
     }
   }
 
